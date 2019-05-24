6ab9d5d479b594d83ffa55897e378b31d326a1df
Jiajie Li <jj@hgeek.net>
2019-05-24 13:39:33 +0800

    Update label.py

--- Route-Marker/label.py
+++ Route-Marker/label.py
@@ -7,11 +7,14 @@ MULTIPLE = 8
 COLOR_POINT = [0, 255, 255]
 COLOR_AVAI = [30, 130, 0]
 COLOR_UNAVAI = [0, 30, 130]
+Graph = [ [0] * 5 for i in range(5)]
+HAVE_START = False
 pnts = []
+
 
 #创建回调函数
 def OnMouseAction(event,x,y,flags,param):
-    global x1, y1, img2, img, pnts
+    global x1, y1, img2, img, img3, pnts, HAVE_START
 
     #Basic Info Display
     img2 = np.zeros((70, 300, 3), np.uint8)
@@ -19,24 +22,44 @@ def OnMouseAction(event,x,y,flags,param):
     cv2.rectangle(img2, (240,15), (250, 25), img[y][x].tolist(), -1)
 
     #Line Info Display
+    if mode == 1 and HAVE_START:
+        img3 = np.zeros((70, 500, 3), np.uint8)
+        cv2.putText(img3, "From X=" + str(x1 // MULTIPLE) + ", Y=" + str(y1 // MULTIPLE) + " No." + str(pnts.index([x1 // MULTIPLE,y1 // MULTIPLE])), (30, 30), font, 0.8, (255, 255, 255))
+        if (img[y][x].tolist() == COLOR_POINT and pnts.count([x // MULTIPLE,y // MULTIPLE])>0):
+            cv2.putText(img3, "To    X=" + str(x // MULTIPLE) + ", Y=" + str(y // MULTIPLE) + " No." + str(pnts.index([x // MULTIPLE,y // MULTIPLE])), (30, 60), font, 0.8, (255, 255, 255))
+        else:
+            cv2.putText(img3, "To", (30, 60), font, 0.8, (255, 255, 255))
+    elif mode == 0:
+        img3 = np.zeros((70, 500, 3), np.uint8)
+        cv2.putText(img3, "Not in Line Mode", (30, 30), font, 0.8, (255, 255, 255))
+    else:
+        img3 = np.zeros((70, 500, 3), np.uint8)
+        cv2.putText(img3, "No Start Point", (30, 30), font, 0.8, (255, 255, 255))
 
     if mode == 1 and event == cv2.EVENT_RBUTTONDOWN:
         if (img[y][x].tolist() == COLOR_POINT):
             print("Line右键点击",img[y][x].tolist())
             x1, y1 = x//MULTIPLE*MULTIPLE, y//MULTIPLE*MULTIPLE
+            HAVE_START = True
 
-    if mode == 1 and event == cv2.EVENT_LBUTTONDOWN:
-        if (img[y][x].tolist() == COLOR_POINT):
+    if mode == 1 and event == cv2.EVENT_LBUTTONDOWN and HAVE_START:
+        if (img[y][x].tolist() == COLOR_POINT and pnts.count([x // MULTIPLE,y // MULTIPLE])>0):
             print("Line左键点击",img[y][x].tolist())
             pts = np.array([[x1,y1],[x1+MULTIPLE,y1+MULTIPLE],[x//MULTIPLE*MULTIPLE+MULTIPLE//2,y//MULTIPLE*MULTIPLE+MULTIPLE//2]], np.int32)
             cv2.fillPoly(img, [pts], (255, 255, 0), 0, 0)
-            #cv2.rectangle(img,,True,(255,255,0),2)
+            Graph[pnts.index([x1 // MULTIPLE,y1 // MULTIPLE])][pnts.index([x // MULTIPLE,y // MULTIPLE])]=1
+            for i in range(len(pnts)):
+                for j in range(len(pnts)):
+                    print(Graph[i][j]," ",end='')
+                print()
 
     if mode == 0 and event == cv2.EVENT_LBUTTONDOWN:
         if (img[y][x].tolist()== COLOR_AVAI):
             print("Point左键点击", img[y][x].tolist())
-            pnts.append([x//MULTIPLE,y//MULTIPLE])
+            if pnts.count([x//MULTIPLE,y//MULTIPLE]) == 0:
+                pnts.append([x//MULTIPLE,y//MULTIPLE])
             print(pnts)
+            #print(Graph[:len(pnts)][:len(pnts)])
             cv2.rectangle(img, (x//MULTIPLE*MULTIPLE, y//MULTIPLE*MULTIPLE), (x//MULTIPLE*MULTIPLE+MULTIPLE, y//MULTIPLE*MULTIPLE+MULTIPLE), COLOR_POINT, -1)
 
     elif mode == 1 and event==cv2.EVENT_MOUSEMOVE and flags ==cv2.EVENT_FLAG_LBUTTON:
@@ -46,8 +69,8 @@ def OnMouseAction(event,x,y,flags,param):
 data = pd.read_csv('map2.csv',header=None).iloc[:,:200]
 print(data.shape)
 img = np.zeros((MULTIPLE*data.shape[0],MULTIPLE*data.shape[1],3),np.uint8)
-
 img2 = np.zeros((70,300,3),np.uint8)
+img3 = np.zeros((70,500,3),np.uint8)
 
 for i in range(data.shape[0]):
     for j in range(data.shape[1]):
@@ -64,11 +87,13 @@ for i in range(data.shape[0]):
 font = cv2.FONT_HERSHEY_SIMPLEX
 cv2.namedWindow('Label')
 cv2.setMouseCallback('Label',OnMouseAction)
-cv2.namedWindow('info')
+cv2.namedWindow('Basic info')
+cv2.namedWindow('Line info')
 
 while(1):
     cv2.imshow('Label',img)
-    cv2.imshow('info',img2)
+    cv2.imshow('Basic info',img2)
+    cv2.imshow('Line info', img3)
     k=cv2.waitKey(1)
     if k==ord('p'):
         mode = 0
